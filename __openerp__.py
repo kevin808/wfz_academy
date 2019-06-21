3f785d805cd7df5546e7b821c584384ebbd90872
Kevin Li <kevin@Kevins-MBP.lan>
2019-06-21 10:57:06 +0800

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Academy",
+
+    'summary': """
+        Learning Odoo development with Kevin,
+        Enjoy the fun with Odoo""",
+
+    'description': """
+        This module covers nearly all sides of a module, from class to reports,
+        it is a good way for you to learn.
+    """,
+
+    'author': "Kevin Lee",
+    'website': "http://kevin808.lofter.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'education',
+    'version': '8.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','board'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        # 'templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        # 'views/session_workflow.xml',
+        'views/session_board.xml',
+        'reports.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
